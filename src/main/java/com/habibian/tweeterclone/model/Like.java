8bd9ff607e8e69ce88ceefde0b770219a3c0e121
Ali Habibian <ali.habibian04@gmail.com>
2023-11-16 19:49:35 +0330

    feat(model): Add Like class for representing likes
    Introduce the Like class representing likes in the TweeterClone application. This entity captures information about likes, including the user who gave the like and the tweet that received the like.

---
+++ src/main/java/com/habibian/tweeterclone/model/Like.java
@@ -0,0 +1,36 @@
+package com.habibian.tweeterclone.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents a like in the application.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "like")
+public class Like {
+    /**
+     * The unique identifier for the like.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    /**
+     * The user who gave the like.
+     */
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    /**
+     * The tweet that received the like.
+     */
+    @ManyToOne
+    @JoinColumn(name = "tweet_id")
+    private Tweet tweet;
+}
