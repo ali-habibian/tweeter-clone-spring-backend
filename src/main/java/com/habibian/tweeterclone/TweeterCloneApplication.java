1f3a3b0c0f8ed352ab52b6a3f18d5a2a490d6d82
Ali Habibian <ali.habibian04@gmail.com>
2023-11-16 17:05:45 +0330

    Initial commit

---
+++ src/main/java/com/habibian/tweeterclone/TweeterCloneApplication.java
@@ -0,0 +1,13 @@
+package com.habibian.tweeterclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habibian/tweeterclone/TweeterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habibian.tweeterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
