debd30eb4add89e48b31af2a88bc0d29bdaac7b0
Ali Habibian <ali.habibian04@gmail.com>
2023-11-16 19:48:00 +0330

    feat(model): Add Tweet class for representing tweets
    Introduce the Tweet class representing tweets in the TweeterClone application. This entity captures various tweet attributes such as content, image, and video. Associations with user, likes, replyTweets, retweetUser, and replyFor are included.

---
+++ src/main/java/com/habibian/tweeterclone/model/Tweet.java
@@ -0,0 +1,81 @@
+package com.habibian.tweeterclone.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a tweet in the TweeterClone application.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "tweet")
+public class Tweet {
+    /**
+     * The unique identifier for the tweet.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    /**
+     * The content of the tweet.
+     */
+    private String content;
+
+    /**
+     * The URL of an image attached to the tweet.
+     */
+    private String image;
+
+    /**
+     * The URL of a video attached to the tweet.
+     */
+    private String video;
+
+    /**
+     * The user who posted the tweet.
+     */
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    /**
+     * The list of likes associated with the tweet.
+     */
+    @OneToMany(mappedBy = "tweet", cascade = CascadeType.ALL)
+    private List<Like> likes = new ArrayList<>();
+
+    /**
+     * The list of reply tweets associated with the tweet.
+     */
+    @OneToMany
+    private List<Tweet> replyTweets = new ArrayList<>();
+
+    /**
+     * The list of users who retweeted the tweet.
+     */
+    @ManyToMany
+    private List<User> retweetUser = new ArrayList<>();
+
+    /**
+     * The tweet to which this tweet is a reply.
+     */
+    @ManyToOne
+    private Tweet replyFor;
+
+    /**
+     * Flag indicating if the tweet is a reply.
+     */
+    private boolean isReply;
+
+    /**
+     * Flag indicating if the object is a tweet.
+     */
+    private boolean isTweet;
+}
