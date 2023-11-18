43c2c6c562526f6fd6c19fcf469c4018b29508ff
Ali Habibian <ali.habibian04@gmail.com>
2023-11-18 17:14:31 +0330

    change SECRET_KEY

--- src/main/java/com/habibian/tweeterclone/config/JwtConstant.java
+++ src/main/java/com/habibian/tweeterclone/config/JwtConstant.java
@@ -5,7 +5,7 @@ package com.habibian.tweeterclone.config;
  */
 public class JwtConstant {
     // Secret key for signing and verifying JWT tokens
-    public static final String SECRET_KEY = "X3vd*d<BIk[s[O)]GQS8kt?FnV&0vc34>M{1s[-5-QFNmSi8qTjd=XVy'&8!-eb>q<rNMQX6Fq_-|X;A";
+    public static final String SECRET_KEY = "[a-zA-z0-9._]^+$Guidelines89797987forAlphabeticalArrayNumeralsAndOtherSymbol$";
 
     // Header key for JWT token in the HTTP request
     public static final String JWT_HEADER = "Authorization";
